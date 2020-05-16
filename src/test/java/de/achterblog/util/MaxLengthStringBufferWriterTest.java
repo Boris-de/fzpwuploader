1632752e7f4e510d5a6ea90b76a09f7355a8e7f8
boris <boris@code.achterblog.de>
2020-05-16 18:47:00 +0200

    upgrade to junit 5.x

--- src/test/java/de/achterblog/util/MaxLengthStringBufferWriterTest.java
+++ src/test/java/de/achterblog/util/MaxLengthStringBufferWriterTest.java
@@ -18,49 +18,52 @@
  */
 package de.achterblog.util;
 
-import static org.hamcrest.Matchers.is;
-
 import java.io.IOException;
-import org.junit.Assert;
-import org.junit.Test;
 
-public class MaxLengthStringBufferWriterTest {
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MaxLengthStringBufferWriterTest {
   @Test
-  public void testZeroLength() throws IOException {
+  void testZeroLength() throws IOException {
     MaxLengthStringBufferWriter w = new MaxLengthStringBufferWriter(0);
     w.append("x");
-    Assert.assertThat(w.toString(), is(""));
+    assertThat(w.toString(), is(""));
   }
 
   @Test
-  public void testSingleCharacter() throws IOException {
+  void testSingleCharacter() throws IOException {
     MaxLengthStringBufferWriter w = new MaxLengthStringBufferWriter(1);
     w.append("x");
-    Assert.assertThat(w.toString(), is("x"));
+    assertThat(w.toString(), is("x"));
     w.append("y");
-    Assert.assertThat(w.toString(), is("y"));
+    assertThat(w.toString(), is("y"));
   }
 
   @Test
-  public void testCutOffIsLineBased() throws IOException {
+  void testCutOffIsLineBased() throws IOException {
     MaxLengthStringBufferWriter w = new MaxLengthStringBufferWriter(10);
     w.append("12345\n6789");
-    Assert.assertThat(w.toString(), is("12345\n6789"));
+    assertThat(w.toString(), is("12345\n6789"));
     w.append("y");
-    Assert.assertThat(w.toString(), is("6789y"));
+    assertThat(w.toString(), is("6789y"));
   }
 
   @Test
-  public void testClose() throws IOException {
+  void testClose() throws IOException {
     MaxLengthStringBufferWriter w = new MaxLengthStringBufferWriter(10);
     w.append("1234");
-    Assert.assertThat(w.toString(), is("1234"));
+    assertThat(w.toString(), is("1234"));
     w.close();
-    Assert.assertThat(w.toString(), is(""));
+    assertThat(w.toString(), is(""));
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testInvalidMaxSize() {
-    new MaxLengthStringBufferWriter(-1);
+  @Test
+  void testInvalidMaxSize() {
+    var e = assertThrows(IllegalArgumentException.class, () -> new MaxLengthStringBufferWriter(-1));
+    assertThat(e.getMessage(), is("maxSize must not be negative"));
   }
 }
